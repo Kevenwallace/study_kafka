adc030ee93b0d026470f7c472e96235875f91f89
keven_wallace <keven.wallace@paraiba.email>
2023-09-20 17:58:09 -0300

    Adiciona o producer e o consumer

--- src/main/java/ecomerce/kafka/NewOrderMain.java
+++ src/main/java/ecomerce/kafka/NewOrderMain.java
@@ -1,7 +1,10 @@
 package ecomerce.kafka;
 
 import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -10,20 +13,31 @@ import org.apache.kafka.common.serialization.StringSerializer;
 public class NewOrderMain {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		var producer = new KafkaProducer<String, String>(properties());
-		var value = "1222333,keven,12234444";
-		ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record);
+		var value = "1222333,keven wallace,12234444";
+		var key = UUID.randomUUID().toString();
+		var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+		var email = "thankyou we are analizin you order";
+		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email,email);
+        Callback callback = (data, ex) -> {
+        	if (ex != null ) {
+        		ex.printStackTrace();
+        		return;
+        	} else {
+        		System.out.println("passou");
+        	}
+        };
+		producer.send(record, callback).get();
+		producer.send(emailRecord, callback).get();
 
 		    }
-		    private static Properties properties() {
-		                 var properties = new Properties();
-		                 properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-		                 properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-		                 properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		                 return properties;
-		          
+    private static Properties properties() {
+         var properties = new Properties();
+         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+         return properties;          
 	}
 
 }
